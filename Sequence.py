20c1b53ff0c40f006714706aefda1ee7fda270ff
Lewis <34522582+GreyJanUK@users.noreply.github.com>
2020-09-02 10:59:53 +0100

    Create Sequence.py

---
+++ Sequence.py
@@ -0,0 +1,5 @@
+x = float(input("Enter voltage")) #step 1
+y = int(input("Enter Ratio Multiplier (example, 41 for 4100MHz)"))#step 2
+z = 100*y # step 3
+print("Your clock speed is",z,"MHz at a voltage of",x,"volts!") #step 4
+
